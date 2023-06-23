cf607b60eaab5f4cfb2b178976d9b1b8df2e7568
星穹铁道高手 <1277526115@qq.com>
2023-06-23 23:06:15 +0800

    修改：     rmdb/src/record/rm_scan.cpp

--- rmdb/src/record/rm_scan.cpp
+++ rmdb/src/record/rm_scan.cpp
@@ -9,6 +9,7 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "rm_scan.h"
+#include "record/bitmap.h"
 #include "rm_file_handle.h"
 
 /**
@@ -16,58 +17,71 @@ See the Mulan PSL v2 for more details. */
  * @param file_handle
  */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
-    // Todo:
-    // 初始化file_handle和rid（指向第一个存放了记录的位置）
-    rid_=Rid{1,0};//从第一页的第0个slot开始
+	// Todo:
+	// 初始化file_handle和rid（指向第一个存放了记录的位置）
+    rid_={1,-1};
+    int start_page=rid_.page_no;
+    int start_slot=rid_.slot_no;
+    for(int i = start_page; i < file_handle_->file_hdr_.num_pages; i++){
+        auto page_handle = file_handle_->fetch_page_handle(i);
+        rid_ = {i, Bitmap::next_bit(true, page_handle.bitmap, page_handle.file_hdr->num_records_per_page, -1)};
+		file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+		return;
+    }
+    rid_ = {file_handle_->file_hdr_.num_pages, -1};
+    return;
 }
 
 /**
  * @brief 找到文件中下一个存放了记录的位置
  */
 void RmScan::next() {
-    // Todo:
-    // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
-    // 获取当前文件的第一个存放数据的页号
-    int start_page = rid_.page_no;
-    int start_slot = rid_.slot_no+1;
-    int max_records=file_handle_->file_hdr_.num_records_per_page;
-    
-    // 遍历页面，找到下一个非空闲的位置
-    for (int page_no = start_page; page_no < file_handle_->file_hdr_.num_pages; ++page_no) {
-        RmPageHandle page_handle = file_handle_->fetch_page_handle(page_no);
-        char* bitmap_temp=page_handle.bitmap;
-        if (page_handle.page_hdr->next_free_page_no==-1) {
-            // 遍历记录槽位，找到下一个非空闲槽位
-            for (int slot = start_slot; slot < max_records; ++slot) {
-                if (bitmap_temp[slot/8]&(1<<(slot%8))==1) {//用位图检查该slot是否不空闲
-                    // 找到非空闲槽位，更新rid_并返回
-                    rid_ = Rid{page_no, slot};
-                    return;
-                }
-            }
-        }
-    }
-    // 若没有找到非空闲位置，则将rid_置为无效
-    rid_ = Rid{-1, -1};
-}
+	// Todo:
+	// 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
 
+	//设置一个标志位来指示是在当前页还是下一页搜寻
+	bool flag = false;
+	// 当前槽位为 -1（表示结束或初始状态），则无需继续执行，函数直接返回
+	if (rid_.slot_no == -1) {
+		return;
+	}
+	int max_pages_num=file_handle_->file_hdr_.num_pages;
+	//for循环扫描
+	for (int i = rid_.page_no; i < max_pages_num; i++) {
+		RmPageHandle page_handle = file_handle_->fetch_page_handle(i);
+		int slot_no = Bitmap::next_bit(true, page_handle.bitmap, page_handle.file_hdr->num_records_per_page, flag ? -1 : rid_.slot_no);
+		int page_end=page_handle.file_hdr->num_records_per_page;
+		//next_bit扫描到了页面末尾仍未找到1位,进入下一次for循环扫描下一页
+		if (slot_no == page_end) {
+			rid_.slot_no = -1;
+			continue;
+		}
+		//nextbit找到了1位
+		else{
+			rid_ = {i, slot_no};
+			file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+			return;
+		}
+		//unpin
+		file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+		//置标志位为1，从本页的slotno而不是-1开始寻找
+		flag = true;
+	}
+	rid_ = {max_pages_num, -1};
+	return;
+}
 
 /**
  * @brief ​ 判断是否到达文件末尾
  */
 bool RmScan::is_end() const {
-    // Todo: 修改返回值
-
-    // 获取文件中最后一个记录的位置
-    Rid last_record = Rid{file_handle_->file_hdr_.num_pages - 1, file_handle_->file_hdr_.num_records_per_page - 1};
-
-    // 判断当前记录的位置是否在最后一个记录之后
-    return (rid_.page_no > last_record.page_no)||((rid_.page_no == last_record.page_no)&&rid_.slot_no>last_record.slot_no);
+	// Todo: 修改返回值
+	return rid_.slot_no == -1;
 }
 
 /**
  * @brief RmScan内部存放的rid
  */
 Rid RmScan::rid() const {
-    return rid_;
+	return rid_;
 }
