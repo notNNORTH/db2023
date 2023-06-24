66a3341ad455b2f2fd876523bf1271940057bd3c
星穹铁道高手 <1277526115@qq.com>
2023-06-24 14:26:49 +0800

    test 4

--- src/storage/buffer_pool_manager.cpp
+++ src/storage/buffer_pool_manager.cpp
@@ -131,7 +131,7 @@ bool BufferPoolManager::unpin_page(PageId page_id, bool is_dirty) {
 
     // 3 根据参数is_dirty，更改P的is_dirty_
     if (is_dirty){
-        page->is_dirty_ = is_dirty;
+        page->is_dirty_ = true;
     }
     
     return true;
