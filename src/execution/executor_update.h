a731cd21a28a717b45df54ef17f8dfa2f558f1fa
星穹铁道高手 <1277526115@qq.com>
2023-06-25 10:44:55 +0800

    在TabMeta中增加(属性名->属性类)的映射，完成update算子

--- src/execution/executor_update.h
+++ src/execution/executor_update.h
@@ -37,10 +37,54 @@ class UpdateExecutor : public AbstractExecutor {
         rids_ = rids;
         context_ = context;
     }
+
     std::unique_ptr<RmRecord> Next() override {
-        
+
+        // 1.检查是否还有待更新的记录。如果没有，返回nullptr表示更新操作完成
+        if (rids_.empty()) {
+            return nullptr;
+        }
+
+        // 2.从rids_中取出最后一个记录的ID
+        Rid rid = rids_.back();
+        rids_.pop_back();
+
+        // 3.通过记录ID使用文件处理器（fh_）获取该记录的内容（RmRecord对象）
+        RmRecord rec = *fh_->get_record(rid, context_);
+
+        // 4.根据set_clauses_中的设定，更新记录的对应字段
+        for (const auto& set_clause : set_clauses_) {
+            auto& col = tab_.ColName_to_ColMeta[set_clause.lhs.col_name];
+            auto& val = const_cast<Value&>(set_clause.rhs);
+
+            if (col.type != val.type) {
+                throw IncompatibleTypeError(coltype2str(col.type), coltype2str(val.type));
+            }
+
+            val.init_raw(col.len);
+            memcpy(rec.data + col.offset, val.raw->data, col.len);
+        }
+
+        // 5.使用文件处理器（fh_）将更新后的记录写回到文件中
+        fh_->update_record(rid, rec.data, context_);
+
+        // 6.针对表中的每个索引，更新相应的索引条目     //////////////////////////////////////////////////////////
+        for (const auto& index : tab_.indexes) {
+            auto ih = sm_manager_->ihs_.at(sm_manager_->get_ix_manager()->get_index_name(tab_name_, index.cols)).get();
+            char* key = new char[index.col_tot_len];
+            int offset = 0;
+
+            for (size_t i = 0; i < index.col_num; ++i) {
+                memcpy(key + offset, rec.data + index.cols[i].offset, index.cols[i].len);
+                offset += index.cols[i].len;
+            }
+
+            // ih->update_entry(key, rid, context_->txn_); /////////////////////////////haven't done//////////////////////////
+        }
+
+        //////////////////////////////////////// return std::make_unique<RmRecord>(std::move(rec));
         return nullptr;
     }
-
+        
     Rid &rid() override { return _abstract_rid; }
 };
